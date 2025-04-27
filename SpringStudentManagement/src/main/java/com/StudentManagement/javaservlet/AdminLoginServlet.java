b69dd8aba82d7affd2095e7904c7350974169b43
Anilchaudhary2810 <150251711+Anilchaudhary2810@users.noreply.github.com>
2025-04-27 15:17:05 +0530

    Add files via upload

---
+++ SpringStudentManagement/src/main/java/com/StudentManagement/javaservlet/AdminLoginServlet.java
@@ -0,0 +1,24 @@
+package com.StudentManagement.javaservlet;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.io.IOException;
+
+public class AdminLoginServlet extends HttpServlet {
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if ((username.equals("anil") && password.equals("anil"))) {
+            HttpSession session = request.getSession();
+            session.setAttribute("admin", "loggedIn");
+
+            response.sendRedirect("AdminDashboardServlet");
+        } else {
+            response.sendRedirect("index.html");
+        }
+    }
+}
